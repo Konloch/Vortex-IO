31627a6cc4d82d32a7c06bcc8a42dd662e8abde1
Konloch <konloch@gmail.com>
2023-03-01 10:01:36 -0700

    Add RT-Echo-Server v1.0.0

---
+++ Return-Carriage-Echo-Server/src/main/java/com/konloch/echo/ConnectedClientData.java
@@ -0,0 +1,12 @@
+package com.konloch.echo;
+
+import java.io.ByteArrayOutputStream;
+
+/**
+ * @author Konloch
+ * @since 3/1/2023
+ */
+public class ConnectedClientData
+{
+	public final ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+}
